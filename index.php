8deab3aed1e5067e99098edabaa034aa8f558eee
radekkrab <radxxx7@mail.ru>
2023-10-04 15:34:49 +0500

    проверка

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/style.css">
-    <title>Document</title>
+    <title>Работа с базой данных</title>
 </head>
 <body>
     <table>
